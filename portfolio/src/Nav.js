7962b01fc83e469730625c0459df471d5bebd776
JespervanVeen <j.vanveen@freepack.nl>
2023-10-05 16:09:41 +0200

    changes

--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -1,17 +1,22 @@
 import logo from './img/logo.png'
 
 const Nav = () => {
-    return (  
-        <div className="nav">
-            <div className="logoarea">
-                <img id="logo" src={logo} alt="lowl" />
+    return (
+        <div className="cover">
+            <div className="nav">
+                <div className="logoarea">
+                    <img id="logo" src={logo} alt="lowl" />
+                </div>
+                <div className="links">
+                    <a href='/'>About me</a>
+                    <a href='/'>Projects</a>
+                </div>
             </div>
-            <div className="links">
-                <a href='/'>About me</a>
-                <a href='/'>Projects</a>
+            <div className="navcenter-area">
+                <p>Hallo</p>
             </div>
-        </div>
-    );
+        </div>  
+    ); 
 }
  
 export default Nav;
