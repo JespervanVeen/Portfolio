b509abeb6f9b0cc9e7126915a90e36af6b1bf032
JespervanVeen <j.vanveen@freepack.nl>
2023-10-13 09:32:09 +0200

    COMMIT
    wdwd

--- portfolio/src/components/Main.js
+++ portfolio/src/components/Main.js
@@ -2,8 +2,15 @@ const Main = () => {
     return (  
         <div className="main-content">
             <div className="main-content-text">
-                <h1 id="titlegreeting">Hello</h1>
-                <h3><br></br>My name is <span style={{color: "#C872ED"}}>Jesper</span>. I am a<br></br>student Software Development.</h3>     
+                <div className="title">
+                    <h1 id="titlegreeting">Hello</h1>
+                </div>
+                <div className="main-text">
+                    <h3 id="titlemain">My name is <span style={{color: "#C872ED"}}>Jesper</span>. I am a<br></br>student Software Development.</h3>     
+                </div>
+                <div className="button-project">
+                    <button id="main-button">Projects</button>
+                </div>
             </div>
         </div>
     );
