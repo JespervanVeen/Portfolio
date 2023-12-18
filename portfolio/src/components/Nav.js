b18aa52d373a9823efd5cce468e6b3c91f8f3928
JespervanVeen <j.vanveen@freepack.nl>
2023-12-18 15:21:13 +0100

    hj

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -1,10 +1,15 @@
 import logo from '../logo.png'
 
 const Nav = ({ scrollTo, scrollToExp }) => {
+    const scrollTop = () => {
+        const top = document.querySelector(".nav");
+        top.scrollIntoView({behavior: 'smooth'})
+    }
+
     return (
             <div className="nav">
                 <div className="logoarea">
-                    <img id="logo" src={logo} alt="lowl" />
+                    <img id="logo" src={logo} alt="lowl" onClick={scrollTop}/>
                 </div>
                 <div className="links">
                     <a onClick={scrollTo}>Projects</a>
